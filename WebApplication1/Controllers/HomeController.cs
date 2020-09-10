a3961fa976cc2efaac5a16b25ec0e69a7b3a1392
Mich Pearson <mich.nstt@gmail.com>
2020-09-10 11:27:45 -0500

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using BrownCow;
+using BrownCowBattalionDiscordBot.Responses;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult DKP()
+        {
+            IEnumerable<Player> players= SheetsQuickstart.ReadEditPlayerDkpSheets.Read().players.AsEnumerable().OrderByDescending(d=>d.dkp);
+
+            return View(players);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
